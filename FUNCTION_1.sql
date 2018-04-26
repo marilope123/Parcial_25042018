35986ef9e20664f758f09fe32e59bd8220818be1
marilope123 <37641253+marilope123@users.noreply.github.com>
2018-04-25 21:44:27 -0500

    Funciones
    Funciones

---
+++ FUNCTION_1.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUNCTION_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "SYSTEM"."FUNCTION_1" (A IN NUMBER, B IN NUMBER, C IN NUMBER)
+RETURN varchar IS
+Z varchar2(20) ;
+    BEGIN
+if B>=c THEN
+	z := A || "/" || A*B || "/" || "ANDRES";
+ELSE 
+	z := A|| "/"  || B*B || "MARTINEZ";
+END IF;
+    RETURN z;
+END FUNCTION_1;
+
+/
---
+++ FUNCTION_2.sql
@@ -0,0 +1,31 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUNCTION_2
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "SYSTEM"."FUNCTION_2" (n POSITIVE) RETURN INTEGER IS
+BEGIN
+	IF (n = 1) OR (n = 2) THEN 
+		RETURN 1;
+	ELSE
+		RETURN fib(n - 1) + fib(n - 2); 
+	END IF;
+END FUNCTION_2;
+
+CREATE OR REPLACE FUNCTION areatriangulo (X IN NUMBER, y IN NUMBER)
+RETURN number IS
+Z number := 0;
+    BEGIN
+        z := (x*y)/2;
+    RETURN z;
+    END;
+
+BEGIN
+        dbms_output.put_Line('Área de triangulo 3 - 4: ' || areatriangulo(3,4));
+END;
+
+ SELECT areatriangulo(3,4) AS MEM FROM DUAL; 
+
+/
